bdd02562a5aa4d7424316a8061c9ffb216a63522
xianling zhang <alchemxz@gmail.com>
2017-02-27 17:26:32 -0500

    Add files via upload

---
+++ CsWriteJson.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+
+public class CsWriteJson : MonoBehaviour {
+    public int[] inc = new int[] { 1, 2, 3, 4, 4 };
+    public Character Player = new Character(0,"Austin", 7, false, new int[] { 7,4,11});
+    //convert the character to json
+
+    JsonData playerJson;
+
+	// Use this for initialization
+	void Start () {
+        playerJson = JsonMapper.ToJson(Player);
+        Debug.Log(playerJson);
+        File.WriteAllText(Application.dataPath + "/Player.json", playerJson.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+public class Character
+{
+    public int id;
+    public string name;
+    public int health;
+    public bool aggressive;
+    public int[] stats;
+
+
+    public Character(int id, string name, int health, bool aggressive, int[] stats)
+    {
+        this.id = id;
+        this.name = name;
+        this.health = health;
+        this.aggressive = aggressive;
+        this.stats = stats;
+    }
+}
---
+++ jsonToCs.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+
+public class RootObject
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public List<int> Stats { get; set; }
+}
+
+
+public class RootObject_
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public List<List<int>> Stats { get; set; }
+}
+public class jsonToCs : MonoBehaviour {
+
+    
+    private string jsonString;
+    private JsonData cmdData;
+
+
+    private string jsonString_;
+    private JsonData cmdData_;
+    // Use this for initialization
+    void Start()
+    {
+        
+        jsonString = File.ReadAllText(Application.dataPath + "/Resources/test.json");
+        RootObject stats = JsonMapper.ToObject<RootObject>(jsonString);
+        //cmdData = JsonMapper.ToObject(jsonString);
+        Debug.Log(stats.Name);
+        Debug.Log(stats.Level);
+        Debug.Log(stats.Stats[0]);
+
+
+        jsonString_ = File.ReadAllText(Application.dataPath + "/Resources/test2.json");
+        RootObject_ stats_ = JsonMapper.ToObject<RootObject_>(jsonString_);
+        Debug.Log(stats_.Stats[0][0]+" " +stats_.Stats[0][1]);
+        Debug.Log(stats_.Stats[1][0] + " " + stats_.Stats[1][1]);
+        Debug.Log(stats_.Stats[2][0] + " " + stats_.Stats[2][1]);
+        cmdData_ = JsonMapper.ToObject(jsonString);
+        
+      
+    }
+}
+
+
